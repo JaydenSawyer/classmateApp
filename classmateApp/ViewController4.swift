9d2e3491666890043b5780f752e4ed61cfa27045
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-12-13 08:08:05 -0600

    Added Edit button funcionality

--- classmateApp/ViewController4.swift
+++ classmateApp/ViewController4.swift
@@ -8,19 +8,29 @@
 import UIKit
 
 class ViewController4: UIViewController {
-    
+    var student: Class2?
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet weak var heightTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        if let student = student {
+            nameTextField.text = student.name
+            yearTextField.text = student.year
+            heightTextField.text = "\(student.height)"
+        }
     }
     
     @IBAction func EditButton2(_ sender: UIButton) {
+        if let student = student {
+            student.name = nameTextField.text ?? ""
+            student.year = yearTextField.text ?? ""
+            if let height = Double(heightTextField.text ?? "") {
+                student.height = height
+            }
+        }
+        
         
     }
-    
-    
 }
